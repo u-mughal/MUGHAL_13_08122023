3b38161c550eeef786bf58f196c09fe42e9c2a89
Usman <usman.mughal@outlook.fr>
2023-12-22 14:06:29 +0100

    Accès à la page User via login et password valide

--- front/src/Pages/Auth/Login.jsx
+++ front/src/Pages/Auth/Login.jsx
@@ -1,27 +1,59 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 import './Login.css'
+import { useState } from "react";
+import axios from "axios";
+import { accountService } from "@/_Services/AccountService";
 
 const Login = () => {
+
+    let navigate = useNavigate();
+    // const [login, setLogin] = useState('');
+    // const [password, setPassword] = useState('');
+
+    const [credentials, setCredentials] = useState({
+        email: 'tony@stark.com',
+        password: 'password123'
+    })
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(credentials);
+        axios.post('http://localhost:3001/api/v1/user/login', credentials)
+            .then(res => {
+                console.log(res)
+                accountService.saveToken(res.data.acces_token)
+                navigate('/user')
+            })
+            .catch(error => console.log(error))
+    }
+    
     return (
         <>
         <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign in</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="input-wrapper">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
+                    <label htmlFor="email">Login</label>
+                    <input type="text" name="email" id="email" value={credentials.email} onChange={onChange} />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input type="password" name="password" id="password" value={credentials.password} onChange={onChange} />
                 </div>
                 <div className="input-remember">
                     <input type="checkbox" id="remember-me" />
                     <label htmlFor="remember-me">Remember me</label>
                 </div>
-                <Link className="sign-in-button" to="/User"> Sign in </Link>
+                <button className="sign-in-button">Sign In</button>
             </form>
         </section>
         </>
--- front/src/_Helpers/AuthGuard.jsx
+++ front/src/_Helpers/AuthGuard.jsx
@@ -1,9 +1,18 @@
-const AuthGuard = () => {
-  return (
-      <div>
-          
-      </div>
-  );
+import { Navigate } from "react-router-dom";
+import { accountService } from "@/_Services/AccountService";
+import PropTypes from "prop-types";
+
+const AuthGuard = ({children}) => {
+
+  if (!accountService.isLogged()) {
+      return <Navigate to="/login"/>
+  }
+
+  return children
+};
+
+AuthGuard.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthGuard;
